e3895473834ba117267b46c6fbe8566e84f9b9e0
luisvinicius09 <luisvinicius0906@gmail.com>
2021-02-22 18:15:24 -0300

    Delete all the file

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
-  has_and_belongs_to_many :events
-end
--- db/migrate/20210222012859_create_users_categories_join_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateUsersCategoriesJoinTable < ActiveRecord::Migration[6.1]
-  def change
-    create_join_table :events, :users do |t|
-      t.index :event_id
-      t.index :user_id
-    end
-  end
-end
