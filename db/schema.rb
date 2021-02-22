fc0e176391a13cf6b56f77029a06ac4ca45c297e
luisvinicius09 <luisvinicius0906@gmail.com>
2021-02-21 22:18:14 -0300

    Add user_id column to event table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_21_230630) do
+ActiveRecord::Schema.define(version: 2021_02_22_011734) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
