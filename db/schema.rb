37b43236ae6a144d4bdc0a11f5371e8b8e288c73
luisvinicius09 <luisvinicius0906@gmail.com>
2021-02-22 23:57:42 -0300

    Migrate DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_024940) do
+ActiveRecord::Schema.define(version: 2021_02_23_025630) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_02_23_024940) do
     t.date "datetime"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
