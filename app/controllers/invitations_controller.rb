d4e3f5f7c891b676298ee5d16e2c2837f97d1bea
luisvinicius09 <luisvinicius0906@gmail.com>
2021-02-24 12:41:49 -0300

    Fix insertion problem

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,7 +1,13 @@
 class InvitationsController < ApplicationController
   def create
     event_id = params[:event_id]
-    @attendee = Invitation.find_or_initialize_by(attendee_id: current_user, attended_event_id: event_id)
-    redirect_to root_path if @attendee.save
+    @attendee = Invitation.find_or_initialize_by(attendee_id: current_user.id, attended_event_id: event_id)
+    if @attendee.save
+      flash[:notice] = 'Now you attended to this event.'
+      redirect_to root_path
+    else
+      flash[:alert] = 'Something wrong happened'
+      redirect_to root_path
+    end
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,4 @@
 class Event < ApplicationRecord
-  # belongs_to :owner, class_name: 'User', foreign_key: 'user_id'
   belongs_to :user
 
   has_many :invitations, foreign_key: :attended_event_id
