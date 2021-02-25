cbcdb49fb8ff159b6133dba8a429b5cde2b5448e
luisvinicius09 <luisvinicius0906@gmail.com>
2021-02-25 19:26:46 -0300

    Fix rubocop errors

--- spec/models/models_spec.rb
+++ spec/models/models_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  context "it checks to see if the user" do
-    it "is valid with valid attributes" do
-      expect(User.new(id: 1, email: 'test@test.com', password: '123456', created_at: Time.now, updated_at: Time.now, name: 'Test')).to be_valid
+  context 'it checks to see if the user' do
+    it 'is valid with valid attributes' do
+      expect(User.new(id: 1, email: 'test@test.com',
+       password: '123456', created_at: Time.now, updated_at: Time.now, name: 'Test')).to be_valid
     end
-    it "is not valid with invalid attributes" do
+    it 'is not valid with invalid attributes' do
       expect(User.new(name: nil, email: nil)).to_not be_valid
     end
   end
@@ -16,12 +17,14 @@ RSpec.describe User, :type => :model do
 end
 
 RSpec.describe Event, :type => :model do
-  context "it checks to see if the event" do
-    it "is valid with valid attributes" do
-      User.new(id: 1, email: 'test@test.com', password: '123456', created_at: Time.now, updated_at: Time.now, name: 'Test')
-      expect(Event.new(id: 1, title: 'Test', body: 'Body of the test', datetime: 2021-02-02, created_at: Time.now, updated_at: Time.now, user_id: nil)).to be_valid
+  context 'it checks to see if the event' do
+    it 'is valid with valid attributes' do
+      User.new(id: 1, email: 'test@test.com', password: '123456',
+       created_at: Time.now, updated_at: Time.now, name: 'Test')
+      expect(Event.new(id: 1, title: 'Test', body: 'Body of the test',
+       datetime: 2021 - 02 - 02, created_at: Time.now, updated_at: Time.now, user_id: nil)).to be_valid
     end
-    it "is not valid with invalid attributes" do
+    it 'is not valid with invalid attributes' do
       expect(Event.new(title: nil, body: nil)).to_not be_valid
     end
   end
