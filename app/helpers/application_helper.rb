e6953b5a72f6f123d6b09c6899f72ddc071d7ec5
luisvinicius09 <luisvinicius0906@gmail.com>
2021-02-25 19:23:04 -0300

    Remove logic from application

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,24 @@ module ApplicationHelper
   def flash_alert
     render inline: '<div class="notification"><p class="alert"><%= alert %></p></div>'.html_safe if flash[:alert]
   end
+
+  def nav_bar(var)
+    if var
+      render inline: '
+      <div class="button">
+        <%= link_to "Profile", edit_user_registration_path %>
+      </div>
+      <div class="button">
+        <%= link_to "Logout", destroy_user_session_path, method: :delete %>
+      </div>'.html_safe
+    else
+      render inline: '
+      <div class="button">
+        <%= link_to "Sign Up", new_user_registration_path %>
+      </div>
+      <div class="button">
+        <%= link_to "Sign In", new_user_session_path %>
+      </div>'.html_safe
+    end
+  end
 end
