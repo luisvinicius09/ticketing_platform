f6afcfcf30b44b501d8ae728e010537848bf74c4
luisvinicius09 <luisvinicius0906@gmail.com>
2021-02-25 12:46:30 -0300

    Fix rubocop error

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,13 +11,13 @@ require 'capybara/rspec'
 ActiveRecord::Migration.maintain_test_schema!
 
 Capybara.register_driver :selenium_chrome do |app|
- Capybara::Selenium::Driver.new(app, browser: :chrome)
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
 end
 
 Capybara.javascript_driver = :selenium_chrome
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = '#{::Rails.root}/spec/fixtures'
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = false
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
