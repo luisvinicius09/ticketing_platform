802dc9e96bb9db0ba307c36fcd56dbc7ceebc7db
luisvinicius09 <luisvinicius0906@gmail.com>
2021-02-25 20:03:39 -0300

    Fix rubocop errors

--- spec/models/models_spec.rb
+++ spec/models/models_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe User, :type => :model do
   context 'it checks to see if the user' do
     it 'is valid with valid attributes' do
       expect(User.new(id: 1, email: 'test@test.com',
-       password: '123456', created_at: Time.now, updated_at: Time.now, name: 'Test')).to be_valid
+                      password: '123456', created_at: Time.now, updated_at: Time.now, name: 'Test')).to be_valid
     end
     it 'is not valid with invalid attributes' do
       expect(User.new(name: nil, email: nil)).to_not be_valid
@@ -22,12 +22,10 @@ RSpec.describe Event, :type => :model do
       User.new(id: 1, email: 'test@test.com', password: '123456',
        created_at: Time.now, updated_at: Time.now, name: 'Test')
       expect(Event.new(id: 1, title: 'Test', body: 'Body of the test',
-       datetime: 2021 - 02 - 02, created_at: Time.now, updated_at: Time.now, user_id: nil)).to be_valid
+                       datetime: 2021 - 02 - 02, created_at: Time.now, updated_at: Time.now, user_id: nil)).to be_valid
     end
     it 'is not valid with invalid attributes' do
       expect(Event.new(title: nil, body: nil)).to_not be_valid
     end
   end
 end
-
-
