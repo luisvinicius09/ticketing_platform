5b0a0506106042f4c0a222edd0c916b21afd898e
luisvinicius09 <luisvinicius0906@gmail.com>
2021-02-26 11:31:03 -0300

    Fix issues raised by TSE

--- app/helpers/invitations_helper.rb
+++ app/helpers/invitations_helper.rb
@@ -1,2 +1,19 @@
 module InvitationsHelper
+  def invitation(var)
+    if var
+      render 'invitations/invitation', event: @event
+    else
+      render inline: '<h3>To attend this event you need to log in!</h3>'.html_safe
+    end
+  end
+
+  def show_member(var)
+    if var
+      render inline: '<h3>No members in this event!</h3>'.html_safe
+    else
+      @event.attendees.each do |attendee|
+        render inline: '<p>' + attendee.name + '</p>'.html_safe
+      end
+    end
+  end
 end
